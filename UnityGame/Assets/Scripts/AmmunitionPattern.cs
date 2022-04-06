a835a6df502b127c62ae5fef2db5bd19b1cdf2c6
azazaatata <tatata1108@mail.ru>
2022-04-06 18:52:18 +0300

    Added ammunition script

---
+++ UnityGame/Assets/Scripts/AmmunitionPattern.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammunition : MonoBehaviour
+{
+    abstract class AmmunitionPattern
+    {
+        protected string _name;
+        protected bool _isDefense;
+        protected int _lifetime;
+        protected int _value;
+        protected int _range;
+
+        public int Lifetime //Определение продолжительности действия предмета
+        {
+            get
+            {
+                return _lifetime;
+            }
+            set
+            {
+                _lifetime += value;
+                //Отправлять сообщение юниту если лайфтайм равен нулю
+            }
+        }
+
+        public string Name => _name;//Название предмета
+        public bool IsDefense => _isDefense;//Переменная определяющая атакующий предмет или защищающий
+        public int Value => _value;//Определение значения(величины) накладываемого бафа
+        public int Range => _range;//Определение дистанции действия абилки
+    }
+
+    class Hemlet : AmmunitionPattern
+    {
+        public Hemlet()
+        {
+            _name = "Шлем";
+            _isDefense = true;
+            _lifetime = 2;
+            _value = 1;
+            _range = 0;
+        }
+    }
+
+    class Shield : AmmunitionPattern
+    {
+        public Shield()
+        {
+            _name = "Щит";
+            _isDefense = true;
+            _lifetime = 1;
+            _value = 2;
+            _range = 0;
+        }
+    }
+
+    class Peak : AmmunitionPattern
+    {
+        public Peak()
+        {
+            _name = "Пика";
+            _isDefense = false;
+            _lifetime = 1;
+            _value = 0;
+            _range = 2;
+        }
+    }
+
+    class Horse : AmmunitionPattern
+    {
+        public Horse()
+        {
+            _name = "Коняшка";
+            _isDefense = false;
+            _lifetime = 2;
+            _value = 1;
+            _range = 1;
+        }
+    }
+
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
