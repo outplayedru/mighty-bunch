52a72e0d209cc67630a621887ebdb00a1541c181
outplayedru <malofeevs00@gmail.com>
2022-04-22 23:39:05 +0300

    PROXY

---
+++ Homeworks/HW7/PROXY/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        interface IHomework
+        {
+            public void DoHomework();  
+        }
+
+        class Homework : IHomework
+        {
+            public void DoHomework()
+            {
+                Console.WriteLine("Пришлось сделать самому.........");
+            }
+        }
+
+        class SnatchHomework : IHomework
+        {
+            Random rand = new Random();
+            Homework homework = new Homework();
+            public void DoHomework()
+            {
+                if (rand.Next(0,2) == 0)
+                {
+                    homework.DoHomework();
+                    return;
+                }
+                else  
+                    Console.WriteLine("Задание выполнено"); 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IHomework homework = new SnatchHomework();
+            homework.DoHomework();
+        }
+    }
+}
