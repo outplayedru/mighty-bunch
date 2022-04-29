d38627743ae74c4b98e7508d1c67331b9121710c
outplayedru <malofeevs00@gmail.com>
2022-04-30 00:49:25 +0300

    Composite v2

--- Homeworks/HW8/COMPOSITE/COMPOSITE/Program.cs
+++ Homeworks/HW8/COMPOSITE/COMPOSITE/Program.cs
@@ -5,29 +5,29 @@ namespace COMPOSITE
 {
     class Program
     {
-        abstract class Component
+        abstract class MMM
         {
             protected string name;
 
-            public Component(string name)
+            public MMM(string name)
             {
                 this.name = name;
             }
 
-            public virtual void Add(Component component) { }
-            public virtual void Remove(Component component) { }
+            public virtual void Add(MMM component) { }
+            public virtual void Remove(MMM component) { }
             public virtual void Print() {Console.WriteLine(name);}
         }
 
-        class Mammoths:Component
+        class Mammoths:MMM
         {
-            private List<Component> list = new List<Component>();
+            private List<MMM> list = new List<MMM>();
             public Mammoths(string name) : base(name){ }
-            public override void Add(Component component)
+            public override void Add(MMM component)
             {
                 list.Add(component);
             }
-            public override void Remove(Component component)
+            public override void Remove(MMM component)
             {
                 list.Remove(component);
             }
@@ -35,19 +35,35 @@ namespace COMPOSITE
             {
                 for (int i = 0; i < list.Count; i++)
                 {
+                    Console.WriteLine(name);
                     list[i].Print();
+                    Console.WriteLine();
                 }
             }
         }
 
-        class Money : Component
+        class Money : MMM
         {
             public Money (string rubles) : base(rubles) { }
         }
 
         static void Main(string[] args)
         {
-            
+            MMM Piramida = new Mammoths("Пирамида");
+            MMM LeonidGolubkov = new Mammoths("Первый участник пирамиды");
+            MMM Ivan = new Mammoths("Друг Леонида");
+            MMM Oleg = new Mammoths("Брат Ивана");
+            MMM LeonidMoney = new Money("100000");
+            MMM IvanMoney = new Money("77777");
+            MMM OlegMoney = new Money("194821");
+            Oleg.Add(OlegMoney);
+            Ivan.Add(Oleg);
+            Ivan.Add(IvanMoney);
+            LeonidGolubkov.Add(Ivan);
+            LeonidGolubkov.Add(LeonidMoney);
+            Piramida.Add(LeonidGolubkov);
+            Piramida.Print();
+
         }
     }
 }
